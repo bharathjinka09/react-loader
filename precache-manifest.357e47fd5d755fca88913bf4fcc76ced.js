e50bc24d3ab385448f519aa9afd00d53776c5252
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-04 12:05:00 +0530

    Updates

---
+++ precache-manifest.357e47fd5d755fca88913bf4fcc76ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed0cc75608cd93d56e5c6bb783d7bcb",
+    "url": "/react-loader/index.html"
+  },
+  {
+    "revision": "f34614022eea9c7fcdd4",
+    "url": "/react-loader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dbe0f11332834c3bc25",
+    "url": "/react-loader/static/js/2.20a85779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-loader/static/js/2.20a85779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34614022eea9c7fcdd4",
+    "url": "/react-loader/static/js/main.5078855a.chunk.js"
+  },
+  {
+    "revision": "858e5a49e7a13026f529",
+    "url": "/react-loader/static/js/runtime-main.503b9ea2.js"
+  }
+]);
